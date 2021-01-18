19ab71c53fcf725ea027844b444531d23978758f
Felipe Monsão <femonsao@gmail.com>
2021-01-18 14:45:28 -0300

    controller,Format, initial events, elements,proto.

---
+++ src/util/Format.js
@@ -0,0 +1,16 @@
+class Format{
+
+
+    static getCamelCase(text){
+
+
+        let div = document.createElement('div');
+
+        div.innerHTML = `<div data-${text}="id"></div>`;
+
+        return Object.keys(div.firstChild.dataset)[0];
+
+
+
+    }
+}
